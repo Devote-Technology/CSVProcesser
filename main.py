2601231cbe51103176d0bb18be2f2ff81db08f1d
Matt Hamp <Matthewhamp3@gmail.com>
2023-09-01 15:07:42 -0600

    Drops error  collums if website is found

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ csv_path = 'csvFiles/orgs-3.csv.errors.csv'
 output_csv_path = 'csvFiles/updatedFile.csv'
 State = "California"
 
+
 def get_domain_from_google(company_name, street_address):
     query = f"{company_name} official website based in {State} with {street_address}"
     try:
@@ -13,6 +14,7 @@ def get_domain_from_google(company_name, street_address):
     except Exception as e:
         print(f"Error while searching: {e}")
         return None
+
 
 try:
     df = pd.read_csv(csv_path)
@@ -26,16 +28,21 @@ except Exception as e:
     print(f"An error occurred while reading the CSV: {e}")
     exit(1)
 
+drop_indices = []
+
 for index, row in df.iterrows():
     company_name = row['Company Name']
     street_address = row['Street Address']
     domain = get_domain_from_google(company_name, street_address)
-    df.at[index, 'Company Domain Name'] = domain
 
-df.drop(['Error code', 'Reason'], axis=1, inplace=True)
+    if domain:  # If a domain is found
+        drop_indices.append(index)
+        df.at[index, 'Company Domain Name'] = domain
+
+df.drop(drop_indices, inplace=True)  # Drop rows
+df.drop(['Error code', 'Reason'], axis=1, inplace=True)  # Drop columns
 
 try:
     df.to_csv(output_csv_path, index=False)
 except Exception as e:
     print(f"An error occurred while writing to the CSV: {e}")
-
