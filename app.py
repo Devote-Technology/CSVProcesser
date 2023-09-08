ead9c4e5d83c6ce62e0e570b1406c4edf2b86cd8
Matt Hamp <Matthewhamp3@gmail.com>
2023-09-08 16:04:22 -0600

    finsihed and working

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
-import base64
 from googlesearch import search
-
-
+from time import sleep
 
 app = Flask(__name__)
 CORS(app)
 
 def get_domain_from_google(company_name, street_address, state):
     query = f"{company_name} official website based in {state} with {street_address}"
+    sleep(2)
     try:
         for j in search(query):
             return j
@@ -32,39 +31,33 @@ def search_domain():
 
 @app.route('/process_csv', methods=['POST'])
 def process_csv():
+    print("Starting Process")
     data = request.json
-    encoded_csv = data.get("orgs", "")
-    decoded_csv = base64.b64decode(encoded_csv)
+    orgs = data.get("orgs", [])
 
-    temp_csv_path = 'temp.csv'
-    output_csv_path = 'updatedFile.csv'
-
-    with open(temp_csv_path, "wb") as f:
-        f.write(decoded_csv)
-
-    try:
-        df = pd.read_csv(temp_csv_path)
-    except FileNotFoundError:
-        return jsonify({"status": "error", "message": f"File {temp_csv_path} not found."}), 400
+    df = pd.DataFrame(orgs)
 
     for index, row in df.iterrows():
+        print(f"Processing row {index}")
         company_name = row['Company Name']
         street_address = row['Street Address']
         state = row['State']
         domain = get_domain_from_google(company_name, street_address, state)
         df.at[index, 'Company Domain Name'] = domain
 
-    df.drop(['Error code', 'Reason'], axis=1, inplace=True)
+    columns_to_drop = ['Error code', 'Reason']
+    for col in columns_to_drop:
+        if col in df.columns:
+            df.drop([col], axis=1, inplace=True)
 
     try:
+        output_csv_path = 'updatedFile.csv'
         df.to_csv(output_csv_path, index=False)
     except Exception as e:
         return jsonify({"status": "error", "message": f"An error occurred while writing to the CSV: {e}"}), 400
 
-    with open(output_csv_path, "rb") as f:
-        encoded_output = base64.b64encode(f.read()).decode()
-
-    return jsonify({"status": "success", "encoded_csv": encoded_output})
+    output_data = df.to_dict('records')
+    return jsonify({"status": "success", "orgs": output_data})
 
 if __name__ == '__main__':
     app.run(debug=True)
